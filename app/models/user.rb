6582b152cacbee4bea733cddc21eaa64d06bb24b
Erich Welz <erichwelz@gmail.com>
2013-12-09 18:42:47 -0500

    New User Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,15 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 3 }
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :city 
+  validates_presence_of :postal_code
 
   validates :email, uniqueness: true
+
+  def full_name
+    full_name = first_name.capitalize + " " + last_name.capitalize 
+  end 
+
 end
