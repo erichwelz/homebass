e68bf8b9f4df46020fa409250896804bceeea3a6
davemerritt <davidmerritt6@gmail.com>
2013-12-12 13:30:20 -0500

    Validations added and updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,19 +9,32 @@ class User < ActiveRecord::Base
   acts_as_taggable
   acts_as_taggable_on :genres, :instruments
 
-  validates :password, length: { minimum: 3 }
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true
-  validates_presence_of :first_name
-  validates_presence_of :last_name
+  validates_confirmation_of :password
+
+  validates_length_of :password, :minimum => 6, :allow_blank => false
+  validates_length_of :first_name, :maximum => 35, :allow_blank => false
+  validates_length_of :last_name, :maximum => 35, :allow_blank => false
+
   validates_presence_of :city 
   validates_presence_of :postal_code
+  validates_presence_of :genre_list
+  validates_presence_of :instrument_list
 
-  validates :email, uniqueness: true
+  validates_format_of :first_name, :with => /[a-z]/
+  validates_format_of :last_name, :with => /[a-z]/
+  validates_format_of :city, :with => /[a-z]/  
+  validates_format_of :postal_code, :with => /\A[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1}[ -]?\d{1}[A-Z]{1}\d{1}\z/
+  
+  validates_uniqueness_of :email
 
+  before_validation :upcase_postal
   after_validation :geocode, :if => :postal_code_changed?
+
+  def upcase_postal
+    self.postal_code.upcase!
+  end
+
   def full_name
     full_name = first_name.capitalize + " " + last_name.capitalize 
-  end 
-    
+  end     
 end
