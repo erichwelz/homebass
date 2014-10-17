81933520b1b16390e71287c7ca51166ae54cce5a
Erich Welz <erichwelz@gmail.com>
2014-10-17 10:16:56 -0400

    starting user_model tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe User do
+  before do
+    @user = User.new(first_name: "Example", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+
+    subject { @user }
+  it { should respond_to(:email) }
+  it { should respond_to(:crypted_password) }
+  it { should respond_to (:salt)}
+  it { should respond_to(:city) }
+  it { should respond_to(:postal_code) }
+  it { should respond_to(:bio) }
+  it { should respond_to(:first_name) }
+  it { should respond_to(:last_name) }
+  it { should respond_to(:remember_me_token) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:longitude) }
+  it { should respond_to(:latitude) }
+  it { should respond_to(:looking_for) }
+  it { should respond_to(:personal_url) }
+
+end
