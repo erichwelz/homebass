66ab88303d6c0539a247b69d0601b4319df7470a
Erich Welz <erichwelz@gmail.com>
2014-10-18 16:26:21 -0400

    initial reference model tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    first_name 'first'
-    last_name 'last'
+    first_name 'user'
+    sequence(:last_name) { |n| '##{n}' }
+    sequence(:email) { |n| 'user_#{n}@example.com' }
     city 'Anytown'
     postal_code 'M1A 2B3'
-    email 'user@example.com'
     password 'foobar'
     password_confirmation 'foobar'
     instrument_list ['InstrumentA','InstrumentB']
     genre_list ['InstrumentA','InstrumentB']
-  end
+   end
 end
