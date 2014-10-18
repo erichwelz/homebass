cf05c437577cc8852df064a97410412d4fdd7bf7
Erich Welz <erichwelz@gmail.com>
2014-10-18 14:50:51 -0400

    wip: adding tests to invitation model

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -3,7 +3,5 @@ class Invitation < ActiveRecord::Base
 	belongs_to :recipient, :foreign_key => "recipient_id", :class_name => "User"
   scope :by_newest, order("created_at DESC")
   default_scope by_newest
-  validates_presence_of :content
-  validates_presence_of :user_id
-  validates_presence_of :recipient_id
+  validates_presence_of :content, :user_id, :recipient_id
 end
