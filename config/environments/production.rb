a77e91f48d6756da4c1900512921f10cbbc7d0b5
Erich Welz <erichwelz@gmail.com>
2014-10-27 12:36:08 -0400

    environment settings with hash rockets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,13 +90,13 @@ Musicianfinder::Application.configure do
   # Change mail delivery to either :smtp, :sendmail, :file, :test
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:         'smtp.sendgrid.net',
-    port:            '587'
-    authentication:  :plain,
-    user_name:       ENV['SENDGRID_USERNAME'],
-    password:        ENV['SENDGRID_PASSWORD'],
-    domain:          'heroku.com',
-    enable_starttls_auto: true
+    :address      => 'smtp.sendgrid.net',
+    :port         => '587'
+    :authentication => :plain,
+    :user_name    => ENV['SENDGRID_USERNAME'],
+    :password     => ENV['SENDGRID_PASSWORD'],
+    :domain       => 'heroku.com',
+    :enable_starttls_auto => true
   }
 
 end
