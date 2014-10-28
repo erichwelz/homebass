d14e3e20b8e12a658c0f01ad94c510b70bbc8d1a
Erich Welz <erichwelz@gmail.com>
2014-10-28 17:48:35 -0400

    passing password into login_user utility

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,6 +1,6 @@
-def login_user
+def login_user(password)
   visit login_path
   within('#login') { fill_in "Email", with: user.email }
-  fill_in "Password", with: user.password
+  fill_in "Password", with: password
   click_button "Login"
 end
