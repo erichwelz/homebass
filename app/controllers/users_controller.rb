f51890620d7199bdb1e4dd00299b90d4c0256310
Erich Welz <erichwelz@gmail.com>
2014-10-29 13:03:18 -0400

    removed stray strong param

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -82,8 +82,8 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:comment,
-                                 :email,
+    params.require(:user)
+                         .permit(:email,
                                  :password,
                                  :password_confirmation,
                                  :first_name,
